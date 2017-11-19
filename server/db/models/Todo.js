d8f2c916fe5e1ded12757e7f984076cca2bd1af1
ev-dev <evan.reed1100@gmail.com>
2017-11-18 19:10:57 -0500

    Setup basic node.js boilerplate

--- server/db/models/Todo.js
+++
@@ -1,9 +0,0 @@
-const { STRING, TEXT } = require('sequelize')
-  , db = require('../db')
-
-const Todo = db.define('todo', {
-  title: STRING,
-  content: TEXT
-})
-
-module.exports = Todo
--- server/db/models/User.js
+++
@@ -1,8 +0,0 @@
-const { STRING } = require('sequelize')
-  , db = require('../db')
-
-const User = db.define('user', {
-  name: STRING
-})
-
-module.exports = User
