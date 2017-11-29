07386045ee5ed13d880a86b61329eb34d31ed839
ev-dev <evan.reed1100@gmail.com>
2017-11-29 10:39:06 -0500

    Update dependencies

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import store from './store'
+import Routes from './routes'
+
+import './styles/index'
+
+const App = () => (
+  <div>
+    <Routes />
+  </div>
+)
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
