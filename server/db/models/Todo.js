52b6fcb30f70a64e0f2eeac8c67f379550b253e1
ev-dev <evan.reed1100@gmail.com>
2017-11-30 07:50:49 -0500

    Update express config

--- server/db/models/Todo.js
+++
@@ -1,7 +0,0 @@
-import { STRING, TEXT } from 'sequelize'
-import db from '../_db'
-
-export const Todo = db.define('todo', {
-  title: STRING,
-  content: TEXT
-})
--- server/db/models/User.js
+++
@@ -1,6 +0,0 @@
-import { STRING } from 'sequelize'
-import db from '../_db'
-
-export const User = db.define('user', {
-  name: STRING
-})
