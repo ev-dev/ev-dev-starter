3d9e22be0a5c49fbe700205d23c443bc3e28cca6
ev-dev <evan.reed1100@gmail.com>
2018-01-15 21:18:21 -0500

    updates to static server to properly serve assets

--- server/app/static.js
+++ server/app/static.js
@@ -1,16 +1,34 @@
 import { Router } from 'express'
 import path from 'path'
+import fs from 'fs'
+
+const filePath = file => path.join(
+  __dirname, '..', '..', 'dist', file
+)
 
 export default Router()
   /* --- Serve React App --- */
   .get('/bundle.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'dist', 'bundle.js'))
+    res.sendFile(filePath('bundle.js'))
   })
+
   /* --- Serve Assets --- */
   .use((req, res, next) => {
-    if (path.extname(req.path).length > 0) res.status(404).end()
-    else next(null)
+    if (path.extname(req.path).length > 0) {
+      const fileRequest = filePath(req.path)
+      fs.stat(fileRequest, (err, status) => {
+        if (err) {
+          console.error('File Not Found...', req.path)
+          res.status(404).end()
+        } else {
+          res.sendFile(fileRequest)
+        }
+      })
+    } else {
+      next(null)
+    }
   })
+  
   /* --- Serve Root HTML --- */
   .get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'))
